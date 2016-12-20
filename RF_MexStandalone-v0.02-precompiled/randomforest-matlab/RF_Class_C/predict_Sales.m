bcce48ff2539d9b7e25b65f61f2d8785fa1d2337
belindee <legna868@163.com>
2016-12-20 16:42:39 +0800

    add new samples and predict_sales.m file.

---
+++ RF_MexStandalone-v0.02-precompiled/randomforest-matlab/RF_Class_C/predict_Sales.m
@@ -0,0 +1,44 @@
+%run plethora of tests
+
+%compile everything
+if strcmpi(computer,'PCWIN') |strcmpi(computer,'PCWIN64')
+   compile_windows
+else
+   compile_linux
+end
+
+clear;
+clc;
+
+total_train_time=0;
+total_test_time=0;
+
+% 
+
+ load data/label55x9
+ load data/smp55
+ 
+% %twonorm, N=300, D=2
+for j=1:10
+	fprintf('%d,',j);
+    for i=1:9
+        
+        % tic用来保存当前时间，而后使用toc来记录程序完成时间。
+        % 两者往往结合使用，用法如下：
+        % tic
+        % operations
+	    tic;
+	    model=classRF_train(smp55(1:54,:),lebel55x9(1:54,i),1000);%右上角的‘表示的是矩阵的转置。
+        total_train_time=toc; 
+        toc
+        tic;
+	    y_hat(:,i) = classRF_predict(smp55(55:55,:),model);
+	    total_test_time=total_test_time+toc;	
+        %length(x0)为数列的长度,即它里面有多少个元素.如果x0是矩阵的话,比方说M行N列,那么length返回M和N这两个数的最大值.
+        %temp=length(find(y_hat~=label59))/length(label59);
+        %keyboard
+    end
+end
+fprintf('\nnum_tree %d: Avg train time %d, test time %d\n',1000,total_train_time/100,total_test_time/100);
+
+
--- RF_MexStandalone-v0.02-precompiled/randomforest-matlab/RF_Reg_C/predict_Sale.m
+++ RF_MexStandalone-v0.02-precompiled/randomforest-matlab/RF_Reg_C/predict_Sale.m
@@ -13,37 +13,42 @@ if strcmpi(computer,'PCWIN') |strcmpi(computer,'PCWIN64')
    compile_windows
 else
    compile_linux
- end  
+end  
+ 
+clear;
+clc
 
-load data/label59
-load data/sam59
+load data/label55x9
+load data/smp55
 
 %diabetes, N=442, D=10
 total_train_time=0;
 total_test_time=0;
 for i=1:10
 	fprintf('%d,',i);
-	tic;
-	model=regRF_train(sam59,label59,1000);
-	total_train_time=toc;
-	tic;
-	y_hat =regRF_predict(sam59,model);
-	total_test_time=total_test_time+toc;	
+    for j=1:9
+	    tic;
+	    model=regRF_train(smp55,lebel55x9(:,j),1000);
+	    total_train_time=toc;
+	    tic;
+	    y_hat(:,j) =regRF_predict(smp55,model);
+	    total_test_time=total_test_time+toc;	
+    end
 end
 fprintf('\nnum_tree %d: Avg train time %d, test time %d\n',1000,total_train_time/100,total_test_time/100);
 
 %diabetes, N=442, D=64
 %this second version of the diabetes dataset has 64 dimensions rather than
 %10
-total_train_time=0;
-total_test_time=0;
-for i=1:1
-	fprintf('%d,',i);
-	tic;
-	model=regRF_train(diabetes.x2,diabetes.y,1000);
-	total_train_time=total_train_time+toc;
-	tic;
-	y_hat =regRF_predict(diabetes.x2,model);
-	total_test_time=total_test_time+toc;	
-end
-fprintf('\nnum_tree %d: Avg train time %d, test time %d\n',1000,total_train_time/100,total_test_time/100);
+% total_train_time=0;
+% total_test_time=0;
+% for i=1:1
+% 	fprintf('%d,',i);
+% 	tic;
+% 	model=regRF_train(diabetes.x2,diabetes.y,1000);
+% 	total_train_time=total_train_time+toc;
+% 	tic;
+% 	y_hat =regRF_predict(diabetes.x2,model);
+% 	total_test_time=total_test_time+toc;	
+% end
+% fprintf('\nnum_tree %d: Avg train time %d, test time %d\n',1000,total_train_time/100,total_test_time/100);
